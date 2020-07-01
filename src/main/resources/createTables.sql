541be50dabc76d628e27a746e1d57ef79aa8e6d2
asdas <zenit-jack@yandex.ru>
2020-07-01 16:02:10 +0300

    add scripts for DB

---
+++ src/main/resources/createTables.sql
@@ -0,0 +1,1 @@
+create table vacancies (id int primary key, vacancyname varchar (100), salary int);
---
+++ src/main/resources/initTables.sql
@@ -0,0 +1,5 @@
+insert into vacancies values (1,'programmer', 350000000);
+insert into vacancies values (2,'programmer', 150000000);
+insert into vacancies values (3,'programmer', 30000);
+insert into vacancies values (4,'manager', 49999);
+insert into vacancies values (5,'cleaner', 15250);
