9fa7533cc7239f4393c7d668fa0236ec29d4c4a3
Arlatven <shulga.antony@gmail.com>
2020-07-26 19:14:17 +0300

    fix PropertyLoader

--- src/main/java/PropertyLoader.java
+++ src/main/java/PropertyLoader.java
@@ -6,7 +6,7 @@ public class PropertyLoader {
 
     public static Properties load(String propertiesPath) throws IOException {
         Properties property = new Properties();
-        InputStream input = PropertyLoader.class.getClassLoader().getResourceAsStream(propertiesPath);
+        InputStream input = PropertyLoader.class.getResourceAsStream(propertiesPath);
         property.load(input);
         return property;
     }
