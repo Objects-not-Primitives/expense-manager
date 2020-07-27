b7ef7cf6a4ab6eba969ee627642f655c81fba4bd
Arlatven <shulga.antony@gmail.com>
2020-07-27 20:07:02 +0300

    fix EmployeeServlet with updated EmployeeService
    add methods from EmployeeDAO to EmployeeService for EmployeeServlet such as getOne, getAll, post, put, delete
    add methods for json converting to EmployeeService from EmployeeServlet
    add method for writing HTTP response to EmployeeService
    most exceptions are now processing in EmployeeService

--- src/main/java/EmployeeService.java
+++ src/main/java/EmployeeService.java
@@ -1,6 +1,14 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class EmployeeService {
+    private static final ObjectMapper mapper = new ObjectMapper();
     private static final String propertiesPath = "application.properties";
 
     private EmployeeDAO employeeDAO;
@@ -14,7 +22,84 @@ public class EmployeeService {
         }
     }
 
-    public EmployeeDAO getEmployeeDAO() {
-        return employeeDAO;
+    public void getOne(String id, HttpServletResponse resp) {
+        try {
+            Employee employee = employeeDAO.selectOne(Integer.parseInt(id)).orElse(null);
+            if (employee == null) {
+                servletWriter("There is no Employee with such id", resp);
+            } else {
+                servletWriter(employeeToJson(employee), resp);
+            }
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+            servletWriter("No connection to database", resp);
+        }
+    }
+
+    public void getAll(HttpServletResponse resp) {
+        try {
+            servletWriter(employeeDAO.selectAll().stream()
+                    .map(EmployeeService::employeeToJson)
+                    .collect(Collectors.joining(System.lineSeparator())), resp);
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+            servletWriter("No connection to database", resp);
+        }
+    }
+
+    public void post(String jsonString, HttpServletResponse resp) {
+        try {
+            employeeDAO.insertRecord(Objects.requireNonNull(jsonToEmployee(jsonString)));
+            servletWriter("New Employee added", resp);
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+            servletWriter("No connection to database", resp);
+        }
+    }
+
+    public void put(String jsonString, HttpServletResponse resp) {
+        try {
+            employeeDAO.updateRecord(Objects.requireNonNull(jsonToEmployee(jsonString)));
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+            servletWriter("No connection to database", resp);
+        }
+    }
+
+    public void delete(int id, HttpServletResponse resp) {
+        try {
+            employeeDAO.deleteRecord(id);
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+            servletWriter("No connection to database", resp);
+        }
+    }
+
+    public void servletWriter(String text, HttpServletResponse resp) {
+        try {
+            resp.getWriter().println(text);
+        } catch (IOException e) {
+            System.out.println("Output problems");
+            e.printStackTrace();
+        }
+    }
+
+    private static String employeeToJson(Employee employee) {
+        try {
+            return mapper.writeValueAsString(employee);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return "Problems encountered when processing (parsing, generating) JSON content";
+        }
+    }
+
+    private static Employee jsonToEmployee(String jsonString) {
+        try {
+            return mapper.readValue(jsonString, Employee.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            System.out.println("Problems encountered when processing (parsing, generating) JSON content");
+            return null;
+        }
     }
 }
--- src/main/java/EmployeeServlet.java
+++ src/main/java/EmployeeServlet.java
@@ -1,85 +1,53 @@
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns = {"/first-servlet/*"})
 public class EmployeeServlet extends HttpServlet {
     private static final EmployeeService employeeService = new EmployeeService();
-    private static final ObjectMapper mapper = new ObjectMapper();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        try {
-            String id = (req.getParameter("id"));
-            if (id.equals("")) {
-                resp.getWriter().println(employeeService.getEmployeeDAO().selectAll().stream()
-                        .map(EmployeeServlet::employeeToJson)
-                        .collect(Collectors.joining(System.lineSeparator())));
-            } else {Employee employee = employeeService.getEmployeeDAO().selectOne(Integer.parseInt(id)).orElse(null);
-                if (employee == null){ resp.getWriter().println("There is no Employee with such id");
-                } else{
-                resp.getWriter().println(employeeToJson(employee));
-                }
-            }
-        } catch (SQLException throwable) {
-            throwable.printStackTrace();
-            resp.getWriter().println("No connection to database");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        String id = (req.getParameter("id"));
+        if (id.equals("")) {
+            employeeService.getAll(resp);
+        } else {
+            employeeService.getOne(id, resp);
         }
     }
 
-    private static String employeeToJson(Employee employee) {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         try {
-            return mapper.writeValueAsString(employee);
-        } catch (JsonProcessingException e) {
+            String jsonString = req.getReader().lines()
+                    .collect(Collectors.joining(System.lineSeparator()));
+            employeeService.post(jsonString, resp);
+        } catch (IOException e) {
+            employeeService.servletWriter("Invalid HTTP request", resp);
             e.printStackTrace();
-            System.out.println("Problems encountered when processing (parsing, generating) JSON content");
-            }
-            return null;
-        }
-
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String str = req.getReader().lines()
-                .collect(Collectors.joining(System.lineSeparator()));
-        try {
-            employeeService.getEmployeeDAO().insertRecord(mapper.readValue(str, Employee.class));
-        } catch (SQLException throwable) {
-            throwable.printStackTrace();
-            resp.getWriter().println("No connection to database");
-        }
-        resp.getWriter().println("Add new vacancy");
-    }
-
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String str = req.getReader()
-                .lines()
-                .collect(Collectors.joining(System.lineSeparator()));
-        try {
-            employeeService.getEmployeeDAO().updateRecord(mapper.readValue(str, Employee.class));
-        } catch (SQLException throwable) {
-            throwable.printStackTrace();
-            resp.getWriter().println("No connection to database");
         }
     }
 
     @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            String jsonString = req.getReader()
+                    .lines()
+                    .collect(Collectors.joining(System.lineSeparator()));
+            employeeService.put(jsonString, resp);
+        } catch (IOException e) {
+            employeeService.servletWriter("Invalid HTTP request", resp);
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
         int id = Integer.parseInt(req.getParameter("id"));
-        try {
-            employeeService.getEmployeeDAO().deleteRecord(id);
-        } catch (SQLException throwable) {
-            throwable.printStackTrace();
-            resp.getWriter().println("No connection to database");
-        }
+        employeeService.delete(id, resp);
     }
 }
 
